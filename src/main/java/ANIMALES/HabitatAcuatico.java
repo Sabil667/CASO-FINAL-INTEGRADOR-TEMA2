08933096cbd3daf5879b4ea5a5b673059cfe79f9
Sabil667 <msabi@myuax.com>
2024-03-06 11:02:12 +0100

    .

--- src/main/java/ANIMALES/HabitatAcuatico.java
+++ src/main/java/ANIMALES/HabitatAcuatico.java
@@ -1,4 +1,18 @@
 package ANIMALES;
 
-public class HabitatAcuatico {
+// Subclase Acuático
+public class HabitatAcuatico extends Habitat {
+    private float temperaturaAgua;
+    private float nivelPH;
+
+    public HabitatAcuatico(String nombre, String ubicacion) {
+        super(nombre, ubicacion);
+    }
+
+    @Override
+    public void monitorearCondiciones() {
+        // Implementación específica para monitorear hábitats acuáticos
+        System.out.println("Monitoreando condiciones del hábitat acuático " + nombre);
+        // Código para monitorear temperatura del agua, nivel de pH, etc.
+    }
 }
