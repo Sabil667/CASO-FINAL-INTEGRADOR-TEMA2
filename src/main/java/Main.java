9ed99ba1a39532c19e59d2a6c0a077b781567f9a
Sabil667 <msabi@myuax.com>
2024-03-06 11:38:43 +0100

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import HABITATS.*;
+import ANIMALES.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,5 +12,30 @@ public class Main {
         habitatAcuatico.monitorearCondiciones();
         habitatTerrestre.monitorearCondiciones();
         aviario.monitorearCondiciones();
+
+
+            // Crear instancias de animales
+            Mamifero leon = new Mamifero("León", "Panthera leo", 5, "Carne", true);
+            Ave aguila = new Ave("Águila", "Aquila chrysaetos", 10, "Carne y pescado", true);
+            Reptil cocodrilo = new Reptil("Cocodrilo", "Crocodylus niloticus", 15, "Carne");
+            Pez pezGlobo = new Pez("Pez Globo", "Tetraodontidae", 3, "Crustáceos y moluscos");
+
+            // Alimentar a los animales
+            leon.alimentar("carne");
+            aguila.alimentar("pescado");
+            cocodrilo.alimentar("pescado");
+            pezGlobo.alimentar("crustáceos");
+
+            // Registrar salud y comportamiento de los animales
+            leon.registrarSalud();
+            leon.registrarComportamiento("El león está descansando bajo la sombra de un árbol.");
+            aguila.registrarSalud();
+            aguila.registrarComportamiento("El águila está volando sobre su territorio.");
+            cocodrilo.registrarSalud();
+            cocodrilo.registrarComportamiento("El cocodrilo está tomando el sol cerca del agua.");
+            pezGlobo.registrarSalud();
+            pezGlobo.registrarComportamiento("El pez globo está nadando en el acuario.");
+        }
     }
-}
+
+
