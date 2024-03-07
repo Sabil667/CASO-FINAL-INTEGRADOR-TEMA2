ec3d61a29a8722ecbfd44dcab2acf5ca17d9162c
Sabil667 <msabi@myuax.com>
2024-03-07 15:36:54 +0100

    -

---
+++ src/main/java/RECURSOS/Recurso.java
@@ -0,0 +1,23 @@
+package RECURSOS;
+
+public class Recurso {
+    private String nombre;
+    private int cantidad;
+
+    public Recurso(String nombre, int cantidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
