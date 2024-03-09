adca1e954992ddfebcf5d6b0a2d6cbc2806b9665
Sabil667 <msabi@myuax.com>
2024-03-09 15:57:33 +0100

    -

---
+++ src/main/java/VISITANTES/GuiaAviario.java
@@ -0,0 +1,8 @@
+package VISITANTES;
+
+public class GuiaAviario {
+    void mostrarInfo() {
+        System.out.println("Bienvenido al recorrido de aves.");
+        // Aquí podrías agregar más detalles sobre el recorrido aviario
+    }
+}
