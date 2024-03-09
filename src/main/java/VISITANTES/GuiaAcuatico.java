5f4ffdcd9b14048e0bc4ae72d4e47f33f6c1c2f4
Sabil667 <msabi@myuax.com>
2024-03-09 15:57:07 +0100

    -

---
+++ src/main/java/VISITANTES/GuiaAcuatico.java
@@ -0,0 +1,8 @@
+package VISITANTES;
+
+public class GuiaAcuatico {
+    void mostrarInfo() {
+        System.out.println("Bienvenido al recorrido de animales acuáticos.");
+        // Aquí podrías agregar más detalles sobre el recorrido acuático
+    }
+}
