9ea35f23b643985900b717d8eac00c586ad0042f
Sabil667 <msabi@myuax.com>
2024-03-09 16:06:10 +0100

    -

---
+++ src/main/java/VISITANTES/GuiaAnimalesAcuaticos.java
@@ -0,0 +1,9 @@
+package VISITANTES;
+
+public class GuiaAnimalesAcuaticos extends GuiaAnimales {
+    @Override
+    void mostrarAnimales() {
+        System.out.println("Animales Acuáticos:");
+        // Aquí mostraría la lista de animales acuáticos disponibles
+    }
+}
