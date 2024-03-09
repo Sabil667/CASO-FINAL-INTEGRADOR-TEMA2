890a33bdd81a21e662ba658a48323ddc283987e1
Sabil667 <msabi@myuax.com>
2024-03-09 16:02:43 +0100

    -

---
+++ src/main/java/VISITANTES/mainVISITANTES.java
@@ -0,0 +1,11 @@
+package VISITANTES;
+
+public class mainVISITANTES {
+    public static void main(String[] args) {
+        GuiaAcuatico guiaAcuatico = new GuiaAcuatico();
+        guiaAcuatico.mostrarInfo();
+
+        GuiaAviario guiaAviario = new GuiaAviario();
+        guiaAviario.mostrarInfo();
+    }
+}
