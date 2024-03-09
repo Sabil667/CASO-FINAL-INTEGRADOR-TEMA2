dac47125451af685c61c51e92f1b20cf71bb3b48
Sabil667 <msabi@myuax.com>
2024-03-09 16:05:37 +0100

    -

---
+++ src/main/java/VISITANTES/GuiaAnimalesTerrestres.java
@@ -0,0 +1,9 @@
+package VISITANTES;
+
+public class GuiaAnimalesTerrestres extends GuiaAnimales {
+    @Override
+    void mostrarAnimales() {
+        System.out.println("Animales Terrestres:");
+        // Aquí mostraría la lista de animales terrestres disponibles
+    }
+}
