ae87cf3206fb3cd7137629e4a901c346f4f8c934
Sabil667 <msabi@myuax.com>
2024-03-09 15:56:21 +0100

    -

---
+++ src/main/java/VISITANTES/Adulto.java
@@ -0,0 +1,8 @@
+package VISITANTES;
+
+public class Adulto extends Visitante {
+    @Override
+    void realizarTour() {
+        System.out.println("¡Bienvenido! Vamos a explorar y aprender sobre los diferentes animales y sus hábitats.");
+    }
+}
