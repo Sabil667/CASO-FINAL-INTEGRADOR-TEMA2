c23903d5d295b5a332377060ad04f66bf1999134
Sabil667 <msabi@myuax.com>
2024-03-09 16:04:15 +0100

    -

---
+++ src/main/java/VISITANTES/TourNiños.java
@@ -0,0 +1,8 @@
+package VISITANTES;
+
+public class TourNiños extends Tour {
+    @Override
+    void comenzar() {
+        System.out.println("¡Bienvenidos niños al Tour Especializado para Niños!");
+    }
+}
