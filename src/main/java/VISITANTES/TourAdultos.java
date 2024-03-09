32c96ebe40e9ced06d80c3285c6452a20100ca0a
Sabil667 <msabi@myuax.com>
2024-03-09 16:04:39 +0100

    -

---
+++ src/main/java/VISITANTES/TourAdultos.java
@@ -0,0 +1,8 @@
+package VISITANTES;
+
+public class TourAdultos extends Tour {
+    @Override
+    void comenzar() {
+        System.out.println("¡Bienvenidos adultos al Tour Especializado para Adultos!");
+    }
+}
