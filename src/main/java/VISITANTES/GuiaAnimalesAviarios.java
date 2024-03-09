7a90a0c7f4a62967ccaf487ba7b4d68467044831
Sabil667 <msabi@myuax.com>
2024-03-09 16:06:42 +0100

    -

---
+++ src/main/java/VISITANTES/GuiaAnimalesAviarios.java
@@ -0,0 +1,9 @@
+package VISITANTES;
+
+public class GuiaAnimalesAviarios extends GuiaAnimales {
+    @Override
+    void mostrarAnimales() {
+        System.out.println("Animales Aviarios:");
+        // Aquí mostraría la lista de animales aviarios disponibles
+    }
+}
