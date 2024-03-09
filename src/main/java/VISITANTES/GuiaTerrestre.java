e96c555683c20bb0f5ecf22ce997ec042f092fe1
Sabil667 <msabi@myuax.com>
2024-03-09 15:56:42 +0100

    -

---
+++ src/main/java/VISITANTES/GuiaTerrestre.java
@@ -0,0 +1,8 @@
+package VISITANTES;
+
+public class GuiaTerrestre {
+    void mostrarInfo() {
+        System.out.println("Bienvenido al recorrido de animales terrestres.");
+        // Aquí podrías agregar más detalles sobre el recorrido terrestre
+    }
+}
